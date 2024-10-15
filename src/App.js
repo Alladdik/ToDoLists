e54ef7e67f9dc6970ae7e12d3cee593289f36c39
Maksim <rudenkomaksim207@gmail.com>
2024-10-15 22:06:41 +0300

    3 commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TodoList from './components/TodoList'; // Підключаємо компонент для списку завдань
+import Register from './components/Register'; // Підключаємо компонент для реєстрації
+import Login from './components/Login'; // Підключаємо компонент для входу
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">Список справ</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Домашня</Nav.Link>
+              <Nav.Link href="/login">Вхід</Nav.Link>
+              <Nav.Link href="/register">Реєстрація</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+
+        <Container className="mt-5">
+          <Switch>
+            <Route path="/" exact component={TodoList} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </Container>
+
+        <footer className="footer bg-dark text-white mt-5 p-3 text-center">
+          &copy; {new Date().getFullYear()} Todo App
+        </footer>
+      </div>
+    </Router>
   );
 }
 
